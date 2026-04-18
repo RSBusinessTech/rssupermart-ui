c0f7be37983a9ed04ffcadfaa78f5e045d5aaa97
RS BusinessTech <rsbusinesstech@gmail.com>
2026-04-18 22:08:37 +0530

    fixing about us

--- src/app/features/about/component/about/about.component.ts
+++ src/app/features/about/component/about/about.component.ts
@@ -11,7 +11,7 @@ export class AboutComponent implements OnInit {
    {
      name: 'Vijay Kumar',
      branch: 'RS SuperMart - Dharamkot Bagga',
-     ownerImage: '/assets/images/Pic1.jpeg',
+     ownerImage: '/assets/images/Pic4.jpeg',
      branchImage: '/assets/images/grocery-banner.jpg',
      description: 'Owner at Dharamkot Bagga Branch of RS SuperMart, committed to quality and customer satisfaction.'
    }
