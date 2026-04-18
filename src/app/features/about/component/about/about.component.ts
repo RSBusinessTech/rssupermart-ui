2b4f652f199c28c8fb18e3057dbb56c32dcb86aa
RS BusinessTech <rsbusinesstech@gmail.com>
2026-04-18 21:48:27 +0530

    Adding About us page

--- src/app/features/about/component/about/about.component.ts
+++ src/app/features/about/component/about/about.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  owners = [
+   {
+     name: 'Vijay Kumar',
+     branch: 'RS SuperMart - Dharamkot Bagga',
+     ownerImage: '/assets/images/Pic1.jpeg',
+     branchImage: '/assets/images/grocery-banner.jpg',
+     description: 'Owner at Dharamkot Bagga Branch of RS SuperMart, committed to quality and customer satisfaction.'
+   }
+ ];
+
   constructor() { }
 
   ngOnInit() {
