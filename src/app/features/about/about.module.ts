23cb755ad4abdff998f9edd46fb920212a5b42a0
RS BusinessTech <rsbusinesstech@gmail.com>
2026-04-19 19:35:20 +0530

    modifying about us

--- src/app/features/about/about.module.ts
+++ src/app/features/about/about.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './component/about/about.component';
+import { SafeUrlPipe } from 'src/app/pipes/safe-url.pipe';
 
 @NgModule({
-  declarations: [AboutComponent],
+  declarations: [AboutComponent,SafeUrlPipe],
   imports: [
     CommonModule,
     AboutRoutingModule   // <--- Import routing module here!
